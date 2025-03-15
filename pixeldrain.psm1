4f2601fb4be846fd0f18cb374221a957758e0223
alain <79479952+chadnpc@users.noreply.github.com>
2025-03-15 21:45:04 +0200

    Update pixeldrain.psm1

--- pixeldrain.psm1
+++ pixeldrain.psm1
@@ -1,34 +1,122 @@
-﻿
-#!/usr/bin/env pwsh
+﻿#!/usr/bin/env pwsh
+
+#Requires -PSEdition Core
+#Requires -Modules PsModuleBase, cliHelper.logger
 #region    Classes
-# Main class
-#class $ModuleName {
-## Define the class. Try constructors, properties, or methods.
-#}
+class PixeldrainHttpException : Exception {
+  PixeldrainHttpException([string]$message) {
+    [Exception]$message
+  }
+}
+
+class Pixeldrain : PsModuleBase, IDisposable {
+  [string]$APIKey
+  [System.Net.Http.HttpClient]$HttpClient
+  [Logger]$Logger
+
+  # Constructor
+  Pixeldrain([string]$apiKey) {
+    $this.APIKey = $apiKey
+    $this.HttpClient = [System.Net.Http.HttpClient]::new()
+    $this.Logger = [Logger]::new()
+  }
+
+  # Constructor with Logger
+  Pixeldrain([Logger]$log, [string]$apiKey = "") {
+    $this.APIKey = $apiKey
+    $this.HttpClient = New-Object System.Net.Http.HttpClient
+    $this.Logger = $log
+  }
+
+  # UploadFile
+  # Uploads a file to pixeldrain and returns its ID.
+  [string] UploadFile([string]$Path) {
+    $endpoint = "https://pixeldrain.com/api/file/" + (Split-Path -Path $Path -Leaf)
+
+    $content = New-Object System.IO.StreamContent -ArgumentList (Get-Content -Path $Path -Encoding Byte)
+    $content.Headers.ContentDisposition = New-Object System.Net.Http.Headers.ContentDispositionHeaderValue -ArgumentList "file" -ArgumentList (Split-Path -Path $Path -Leaf)
+
+    $request = New-Object System.Net.Http.HttpRequestMessage
+    $request.Method = New-Object System.Net.Http.HttpMethod("PUT")
+    $request.RequestUri = New-Object Uri($endpoint)
+    $request.Content = $content
+
+    try {
+      $response = $this.HttpClient.SendAsync($request).Result
+      $response.EnsureSuccessStatusCode()
+
+      $responseBody = $response.Content.ReadAsStringAsync().Result
+      $jsonObject = ConvertFrom-Json $responseBody
+
+      $id = $jsonObject.id
+      $this.Logger.LogDebug("Successfully uploaded $($Path) and received an ID of $($id).")
+      return $id
+    } catch {
+      $errorMessage = $_.Exception.Message
+      $this.Logger.LogError("Error uploading file: $($errorMessage)")
+      throw $_
+    }
+  }
+
+  # DownloadFile
+  # Downloads a file from pixeldrain and returns a byte array containing the file.
+  [byte[]] DownloadFile([string]$ID, [string]$Path = "") {
+    $endpoint = "https://pixeldrain.com/api/file/" + $ID
+
+    $request = New-Object System.Net.Http.HttpRequestMessage
+    $request.Method = New-Object System.Net.Http.HttpMethod("GET")
+    $request.RequestUri = New-Object Uri($endpoint)
+
+    try {
+      $response = $this.HttpClient.SendAsync($request).Result
+      $response.EnsureSuccessStatusCode()
+
+      $byteArray = $response.Content.ReadAsByteArrayAsync().Result
+
+      if (-not [string]::IsNullOrEmpty($Path)) {
+        try {
+          [System.IO.File]::WriteAllBytes($Path, $byteArray)
+        } catch {
+          $this.Logger.LogError("Error saving file to disk: $($_.Exception.Message) : $($_.Exception.InnerException)")
+        }
+      }
+      $this.Logger.LogDebug("Successfully downloaded file $($ID)$(([string]::IsNullOrWhiteSpace($Path) ? " and probably saved it to $($Path)" : [string]::Empty))")
+      return $byteArray
+    } catch {
+      $errorMessage = $_.Exception.Message
+      $this.Logger.LogError("Error downloading file: $($errorMessage)")
+      throw $_
+    }
+  }
+
+  [void] Dispose() {
+    $this.HttpClient.Dispose()
+  }
+}
 #endregion Classes
 # Types that will be available to users when they import the module.
 $typestoExport = @(
-    #[$ModuleName]
+  #[$ModuleName]
 )
 $TypeAcceleratorsClass = [PsObject].Assembly.GetType('System.Management.Automation.TypeAccelerators')
 foreach ($Type in $typestoExport) {
-    if ($Type.FullName -in $TypeAcceleratorsClass::Get.Keys) {
-        $Message = @(
-            "Unable to register type accelerator '$($Type.FullName)'"
-            'Accelerator already exists.'
-        ) -join ' - '
-        "TypeAcceleratorAlreadyExists $Message" | Write-Debug
-    }
+  if ($Type.FullName -in $TypeAcceleratorsClass::Get.Keys) {
+    $Message = @(
+      "Unable to register type accelerator '$($Type.FullName)'"
+      'Accelerator already exists.'
+    ) -join ' - '
+    "TypeAcceleratorAlreadyExists $Message" | Write-Debug
+  }
 }
 # Add type accelerators for every exportable type.
 foreach ($Type in $typestoExport) {
-    $TypeAcceleratorsClass::Add($Type.FullName, $Type)
+  $TypeAcceleratorsClass::Add($Type.FullName, $Type)
 }
 # Remove type accelerators when the module is removed.
 $MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
-    foreach ($Type in $typestoExport) {
-        $TypeAcceleratorsClass::Remove($Type.FullName)
-    }
+  foreach ($Type in $typestoExport) {
+    $TypeAcceleratorsClass::Remove($Type.FullName)
+  }
 }.GetNewClosure();
 
 $scripts = @();
@@ -37,21 +125,19 @@ $scripts += Get-ChildItem "$PSScriptRoot/Private" -Filter "*.ps1" -Recurse -Erro
 $scripts += $Public
 
 foreach ($file in $scripts) {
-    Try {
-        if ([string]::IsNullOrWhiteSpace($file.fullname)) { continue }
-        . "$($file.fullname)"
-    }
-    Catch {
-        Write-Warning "Failed to import function $($file.BaseName): $_"
-        $host.UI.WriteErrorLine($_)
-    }
+  Try {
+    if ([string]::IsNullOrWhiteSpace($file.fullname)) { continue }
+    . "$($file.fullname)"
+  } Catch {
+    Write-Warning "Failed to import function $($file.BaseName): $_"
+    $host.UI.WriteErrorLine($_)
+  }
 }
 
 $Param = @{
-    Function = $Public.BaseName
-    Cmdlet   = '*'
-    Alias    = '*'
-    Verbose  = $false
+  Function = $Public.BaseName
+  Cmdlet   = '*'
+  Alias    = '*'
+  Verbose  = $false
 }
 Export-ModuleMember @Param
-      
